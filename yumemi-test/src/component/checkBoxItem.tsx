d414323b8613027a001d38691bac40f579fd51c3
489yuki <yuuki.s.mail10@gmail.com>
2021-12-31 18:05:49 +0900

    revise:material-UIからデフォルトに変更。

--- yumemi-test/src/component/checkBoxItem.tsx
+++ yumemi-test/src/component/checkBoxItem.tsx
@@ -1,15 +1,11 @@
-import { Checkbox, FormControlLabel, Grid } from "@material-ui/core"
-
 const CheckBoxItem:React.FC<{checked:boolean,prefCode:number,prefName:string,handleChange:any}>= (props) => {
     return (
-        <Grid item>
-        <FormControlLabel
-            control={
-              <Checkbox checked={props.checked} onChange={props.handleChange} name={`${props.prefCode}`} />
-            }
-            label={props.prefName}
-          />
-          </Grid>
+        <>
+        <div>
+        <input type="checkbox" checked={props.checked} onChange={props.handleChange} name={`${props.prefCode}`}/>
+        <span>{props.prefName}</span>
+        </div>
+        </>
     )
 }
 
